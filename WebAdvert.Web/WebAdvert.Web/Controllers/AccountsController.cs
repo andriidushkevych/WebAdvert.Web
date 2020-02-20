3c051d2c6feba9142a1c99d3e64e9b0a8f4b16bd
Andrii Dushkevych <adushkevych@logisense.com>
2020-02-19 22:51:30 -0500

    Initial commit. Signup and confirm email functionality addded

---
+++ WebAdvert.Web/WebAdvert.Web/Controllers/AccountsController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.AspNetCore.Identity.Cognito;
+using Amazon.Extensions.CognitoAuthentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebAdvert.Web.Models.Accounts;
+
+namespace WebAdvert.Web.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly SignInManager<CognitoUser> _signInManager;
+        private readonly UserManager<CognitoUser> _userManager;
+        private readonly CognitoUserPool _pool;
+
+        public AccountsController(SignInManager<CognitoUser> signInManager, UserManager<CognitoUser> userManager,
+            CognitoUserPool pool)
+        {
+            _pool = pool;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public async Task<IActionResult> Signup()
+        {
+            var model = new SignupModel();
+            return View(model);
+        }
+
+        public async Task<IActionResult> Confirm()
+        {
+            var model = new ConfirmModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Signup(SignupModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _pool.GetUser(model.Email);
+                if (user.Status != null)
+                {
+                    ModelState.AddModelError("UserExists", "This email is already in use");
+                    return View(model);
+                }
+                user.Attributes.Add(CognitoAttribute.Name.AttributeName, model.Email);
+                var createdUser = await _userManager.CreateAsync(user, model.Password);
+                if (createdUser.Succeeded)
+                {
+                    return RedirectToAction("Confirm");
+                }
+                foreach (var error in createdUser.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("Confirm")]
+        public async Task<IActionResult> ConfirmPost(ConfirmModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                 var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user == null)
+                {
+                    ModelState.AddModelError("NotFound", "User with specified email was not found");
+                }
+
+                var result = await (_userManager as CognitoUserManager<CognitoUser>).ConfirmSignUpAsync(user, model.Code, true);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ WebAdvert.Web/WebAdvert.Web/Models/Accounts/SignupModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAdvert.Web.Models.Accounts
+{
+    public class SignupModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(6, ErrorMessage = "Password must be at least 6 chars long")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords must match")]
+        [StringLength(6, ErrorMessage = "Password must be at least 6 chars long")]
+        [Display(Name = "Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
